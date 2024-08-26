b60a434c1874eed7cb5706f6cbbffdb86fe479f3
Ibrahim Shendy <4webcoding@gmail.com>
2024-08-26 13:50:47 +0300

    Change root route and add footer section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#article", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "articles#index"
 
   # Specify custom routes
   # get "articles", to: "articles#index"
