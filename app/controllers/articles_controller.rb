497d911943ff915af543bab48b0ffe7fdf3a7ad5
Ibrahim Shendy <4webcoding@gmail.com>
2024-08-30 15:19:03 +0300

    Add scope for return only latest articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all;
+    @articles = Article.latest;
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,6 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  scope :latest, -> { order(created_at: :desc).limit(10) }
 end
